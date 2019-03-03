a93ed8c98eaa31c18947382f55d3d79a6af44487
Joaoviana <joaopadraoviana@gmail.com>
2019-03-03 13:55:45 +0000

    added go back buttons to default menu and selection menu.

--- src/scripts/utilsComponents.js
+++ src/scripts/utilsComponents.js
@@ -31,3 +31,48 @@ AFRAME.registerComponent('animate-menu-item-on-hover', {
         });
     }
 });
+
+AFRAME.registerComponent('go-back', {
+    init: function () {
+        this.el.addEventListener('click', function (evt) {
+            let sceneEl = document.querySelector("a-scene");
+            let mask = sceneEl.querySelector("#mask");
+            if(this.id == 'go-back-selection-menu') {
+                console.log(mask);
+                mask.setAttribute(
+                    "template",
+                    "src",
+                    "./src/templates/selectionMenu/selectionMenu.template"
+                );
+            } else if(this.id == 'go-back-main-menu') {
+                console.log(mask);
+                mask.setAttribute(
+                    "template",
+                    "src",
+                    "./src/templates/mainMenu/mainMenu.template"
+                );
+            }
+        });
+    }
+});
+
+
+// let sceneEl = document.querySelector("a-scene");
+//       let mask = sceneEl.querySelector("#mask");
+//       // let selectionMenu = sceneEl.querySelector('#selection-menu');
+//       // let defaultMenu = sceneEl.querySelector('#default-menu');
+//       // let customiseMenu = sceneEl.querySelector('#customise-menu');
+//       // let learnMenu = sceneEl.querySelector('#learn-menu');
+//       let menuSelected = this.childNodes[1].components.text.attrValue.value;
+
+//       //hide selectionMenu
+//       // selectionMenu.setAttribute("visible", 'false');
+
+//       //set attribute visible to default-menu customise-menu and learn-menu
+//       if (menuSelected == "default") {
+//         // defaultMenu.setAttribute("visible", 'true');
+//         mask.setAttribute(
+//           "template",
+//           "src",
+//           "./src/templates/defaultMenu/defaultMenu.template"
+//         );
