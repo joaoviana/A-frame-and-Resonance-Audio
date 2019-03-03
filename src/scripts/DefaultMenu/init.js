0e063920245a2371b68ae320f92d9e4b22ee49f3
Joaoviana <joaopadraoviana@gmail.com>
2019-03-03 13:21:19 +0000

    added init files for each menu

---
+++ src/scripts/DefaultMenu/init.js
@@ -0,0 +1,116 @@
+let audioContext;
+let scene;
+let x;
+let xSource;
+let source;
+let audioReady = false;
+
+function initAudioDefault() {
+  // Set room acoustics properties.
+  let defaultDimensions = {
+    width: 20,
+    height: 20,
+    depth: 20
+  };
+
+  let defaultMaterial = setAllRoomProperties("transparent");
+
+  audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+  // Create a (1st-order Ambisonic) ResonanceAudio scene.
+  scene = new ResonanceAudio(audioContext);
+
+  // Send scene's rendered binaural output to stereo out.
+  scene.output.connect(audioContext.destination);
+
+  scene.setRoomProperties(defaultDimensions, defaultMaterial);
+
+  //can add this into another function no?
+  // Create an audio element. Feed into audio graph.
+  x = document.createElement("audio");
+  x.src = "./soundFiles/funky.mp3";
+  x.crossOrigin = "anonymous";
+  x.load();
+
+  xSource = audioContext.createMediaElementSource(x);
+
+  // Create a Source, connect desired audio input to it.
+  source = scene.createSource();
+  xSource.connect(source.input);
+  audioReady = true;
+}
+
+// a-frame components relating to the Resonance Audio SDK and audio context
+AFRAME.registerComponent("register-room-property", {
+  init: function() {
+    this.el.addEventListener("click", function(evt) {
+      let dimensions = 20;
+      let material = this.getAttribute("src").replace("#", "");
+      scene.setRoomProperties(
+        setAllRoomDimensions(dimensions),
+        setAllRoomProperties(material)
+      );
+    });
+  }
+});
+
+AFRAME.registerComponent("listener", {
+  init() {
+    this.cameraMatrix4 = new AFRAME.THREE.Matrix4();
+  },
+  tick: function() {
+    this.cameraMatrix4 = this.el.object3D.matrixWorld;
+    if (scene) scene.setListenerFromMatrix(this.cameraMatrix4);
+  }
+});
+
+AFRAME.registerComponent("default-menu-sound-source", {
+  init: function() {
+    this.wpVector = new THREE.Vector3();
+    var isPlaying = false;
+    
+    this.el.addEventListener("click", function() {
+      this.setAttribute("color", "pink");
+      if (audioContext) audioContext.resume();
+
+      if (isPlaying == false && x) {
+        x.play();
+        isPlaying = true;
+      } else if (isPlaying == true && x) {
+        x.pause();
+        isPlaying = false;
+      }
+    });
+  },
+
+  tick: function() {
+    if (source) {
+      source.setPosition(
+        this.el.object3D.getWorldPosition(this.wpVector).x,
+        this.el.object3D.getWorldPosition(this.wpVector).y,
+        this.el.object3D.getWorldPosition(this.wpVector).z
+      );
+    }
+  }
+});
+
+// setAmbisonicOrder(order)
+
+// /**
+//  * Set the listener's position and orientation using a Three.js Matrix4 object.
+//  * @param {Object} matrix
+//  * The Three.js Matrix4 object representing the listener's world transform.
+//  */
+// ResonanceAudio.prototype.setListenerFromMatrix = function(matrix) {
+//   this._listener.setFromMatrix(matrix);
+
+//   // Update the rest of the scene using new listener position.
+//   this.setListenerPosition(this._listener.position[0],
+//     this._listener.position[1], this._listener.position[2]);
+// };
+
+// .setSpeedOfSound = function(speedOfSound) {
+
+//for tomorrow: make a scene that will take those room property parameters.
+//consider a menu before the room properties one that you are able to set the speed of sound, ambisonic order, and maybe add more room wall props, maybe onnnnnnnnnn the menu before choosing the each wall one.
+//idk, think about it
---
+++ src/scripts/SelectionMenu/init.js
@@ -0,0 +1,32 @@
+AFRAME.registerComponent("select-menu-box", {
+  init: function() {
+    this.el.addEventListener("click", function(evt) {
+      let sceneEl = document.querySelector("a-scene");
+      let mask = sceneEl.querySelector("#mask");
+      // let selectionMenu = sceneEl.querySelector('#selection-menu');
+      // let defaultMenu = sceneEl.querySelector('#default-menu');
+      // let customiseMenu = sceneEl.querySelector('#customise-menu');
+      // let learnMenu = sceneEl.querySelector('#learn-menu');
+      let menuSelected = this.childNodes[1].components.text.attrValue.value;
+
+      //hide selectionMenu
+      // selectionMenu.setAttribute("visible", 'false');
+
+      //set attribute visible to default-menu customise-menu and learn-menu
+      if (menuSelected == "default") {
+        // defaultMenu.setAttribute("visible", 'true');
+        mask.setAttribute(
+          "template",
+          "src",
+          "./src/templates/defaultMenu/defaultMenu.template"
+        );
+        initAudioDefault();
+        //
+      } else if (menuSelected == "customise") {
+        //
+      } else if (menuSelected == "learn") {
+        //
+      }
+    });
+  }
+});
